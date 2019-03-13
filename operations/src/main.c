dfb9b52395505709717aac0ee6945e4db0e49d36
Caroline G <caroline@galliez.com>
2019-03-13 15:08:54 +0100

    multiplication vérifiée + commentaires

--- operations/src/main.c
+++ operations/src/main.c
@@ -48,10 +48,7 @@ int main(void){
 	int B[5] = {0b00000000000000000000000000000010, 0b00000000000000001000000000000000, 0b01000000000000000000000000000100, 0b00000000010000000000000000100000, 0b00100000010000000000100000000001};
 	int C[5] = {0b00000000000000000000000000100010, 0b00000100000000000000000000000000, 0b01000000000000000000000000000100, 0b00000000000000000000000000000000, 0b00000000000000000000000000000001};
 	int tab_res[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	//printf("size = %d ", sizeof(tab_res)/sizeof(tab_res[0]));
 	mult_128(A, B, tab_res);
-	//sub(tab_res, 3, A, B, C);
-	//printf("resulat = %d, %d, %d, %d, %d\n", tab_res[0],tab_res[1],tab_res[2],tab_res[3], tab_res[4] );
 
 	fflush(stdout);
 
@@ -159,19 +156,24 @@ void mult_128(int* A, int* B, int* result) {
 	int64_t tempo;
 	int tab[25][10] = {0};
 	int compteur = 0;
+	// On fait : A0 * B0, A1 * B0, A2 * B0, ... A4 * B4.
 	for (int i = 4; i >= 0; i--) {
 		for (int j = 4; j >= 0; j--) {
 			tempo = (int64_t)A[j]*(int64_t)B[i];
+			// Pour chaque multiplication, on récupère les bits de poids forts et les bits de poids faibles
 			tab[compteur][9+i+j-8] = tempo & 0x7FFFFFFF; // Récupération des bits de poids faibles en mettant les bits de poids forts à 0
 			tab[compteur][9+i+j-9] = tempo >> 31; // Récupération des bits de poids forts en décalant vers la gauche
-			//printf("i = %d, j = %d, A = %d, B = %d, tempo = %I64d, poids faibles = %d, poids forts = %d\n", i, j, A[j], B[i], tempo, tab[compteur][9+i+j-8], tab[compteur][9+i+j-9] );
 			compteur += 1;
 		}
 	}
+	// On additionne les différents résultats intérmédiaires
+	// Par exemple, (A0B0)poids faible + 0, puis (A0*B0*)poids fort + (A1*B0)poids faible + (A0*B1)poids faible, ...
 	int tempCarry = 0;
 	int tempResult = 0;
 	int carry = 0;
-	result = (int [10]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // On réinitialise R au cas où
+	// Le résultat sera stocké dans l'array result
+	result = (int [10]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // On réinitialise le tableau contenant le résultat au cas où
+	// On réalise les différentes additions
 	for (int k = 9; k >= 0; k--) {
 		result[k] = carry;
 		carry = 0;
@@ -179,24 +181,14 @@ void mult_128(int* A, int* B, int* result) {
 			add_32(&result[k], &tab[l][k], &result[k], &tempCarry);
 			carry = carry + tempCarry;
 			tempCarry = 0;
-			//add_32(&tempResult, &result[k], &result[k], &tempCarry);
-			//printf(" k = %d, l = %d, tab = %d, carry = %d, result = %d\n", k, l, tab[l][k], carry, result[k]);
-			//carry = carry + tempCarry;
 		}
 	}
-//	for (int i = 0; i < sizeof(tab)/sizeof(tab[0]); i++) {
-//		//printf("i = %d\n", i);
-//		for (int j = 0; j < sizeof(tab[0])/sizeof(tab[0][0]); j++) {
-//			printf("%d ", tab[i][j]);
-//		}
-//		printf("\n");
-//	}
-
-	//printf("size = %d ", sizeof(result)/sizeof(result[0]));
+	// Affichage du résultat
 	for (int i = 0; i < 10; i++)
 		printf("%d ", result[i]);
 }
 
+// Fonction aide pour afficher matrice
 //void printDoubleArray(int** A) {
 //	for (int i = 0; i < sizeof(A)/sizeof(A[0]); i++) {
 //		printf("i = %d\n", i);
