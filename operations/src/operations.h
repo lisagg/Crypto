4f7cb3317bb8638aa3cfbb90e63d8684b3a1864e
Caroline G <caroline@galliez.com>
2019-03-14 08:56:10 +0100

    Découpage en plusieurs fichiers

---
+++ operations/src/operations.h
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include<stdio.h>
+
+void add_128(int* A, int* B, int* result);
+void add_32(int* A, int* B, int * result, int* carry);
+void add(int *tab_res, int num,...);
+int isBiggerOrEqual(int* A, int* B);
+void sub_128(int* A, int* B, int* result);
+void sub(int *tab_res, int num,...);
+void mult_128(int* A, int* B, int* result);
+//void printDoubleArray(int* A);
